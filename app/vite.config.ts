0810e8cb37df2f2392fdc9d958bf095e462e835c
nakano <nakano@sra.co.jp>
2026-07-19 13:30:15 +0000

    Add GitHub Pages deployment workflow

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/202607_SBI_Portfolio/',
   plugins: [
     vue(),
     vueDevTools(),
